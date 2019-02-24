6c98a2810bc05bff202e8c23cdd64fdd20ea1bd4
veslie losius <veslielosius@yahoo.com>
2019-02-24 23:21:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,9 @@ def words_five_letters_long(text)
 text.scan(/\b[a-zA-Z]{5}\b/)  
 end
 
-def first_word_capitalized_and_ends_with_punctuatio?(text)
+def first_word_capitalized_and_ends_with_punctuation?(text)
 text.
+text.match()
 end
 
 def valid_phone_number?(phone)
