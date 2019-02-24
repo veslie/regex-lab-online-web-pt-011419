e31c6694b19478853868e3555d0bfea575f2f57c
veslie losius <veslielosius@yahoo.com>
2019-02-24 21:02:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,11 +4,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.match(/\A["un"]\z["ing"]/) ? true : false 
+text.match(/\A["un"]\z["ing"]/) 
 end
 
 def words_five_letters_long(text)
-text.scan(/w{5}[a-zA-Z]/) ? true : false 
+text.scan(/w{5}[a-zA-Z]/)  
 end
 
 def first_word_capitalized_and_ends_with_punctuatio?(text)
@@ -16,5 +16,5 @@ text.match(/\A[A-Z]\z\W\D/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.scan(/\d[0-9]/) ? true : false 
+phone.scan(/\d[0-9]/)  
 end
