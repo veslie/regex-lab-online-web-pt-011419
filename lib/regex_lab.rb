af27d464bc244b58e7424834f43c3c3d8b40e290
veslie losius <veslielosius@yahoo.com>
2019-02-24 19:46:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+.scane(/)
 end
 
 def words_five_letters_long(text)
-
+.grep(/^\w{5}\$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
