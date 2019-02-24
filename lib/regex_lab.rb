ad61349a5065584f0c119e2e1b1c72fa2ffc338f
veslie losius <veslielosius@yahoo.com>
2019-02-24 21:35:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.grep(/\A[un]\z[ing]/) 
+text.scan(/\A[un]\z[ing]/) 
 end
 
 def words_five_letters_long(text)
