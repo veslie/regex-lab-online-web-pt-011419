d23c55b0a2ed23144ccc04b594559b5f2160879a
veslie losius <veslielosius@yahoo.com>
2019-02-24 21:17:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,11 +4,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.grep(/\A["un"]\z["ing"]/) ? true : false 
+text.grep(/\A[un]\z[ing]/) 
 end
 
 def words_five_letters_long(text)
-text.scan(/w{5}[a-zA-Z]/)  
+text.grep(/w{5}[a-zA-Z]/)  
 end
 
 def first_word_capitalized_and_ends_with_punctuatio?(text)
